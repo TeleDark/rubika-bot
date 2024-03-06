8097b186a04dd9c9277859646825540840eb7f11
Tele Dark <teledark0978@gmail.com>
2024-03-06 12:21:24 +0000

    improved organization and readability

--- stop.py
+++ stop.py
@@ -1,24 +1,42 @@
 import os
 from colorama import Fore, Style
+
 GREEN = Fore.GREEN
 PLAIN = Style.RESET_ALL
 
-file_name = None
-for i in os.listdir():
-    if i.endswith(".py") and i.startswith("bot_"):
-        file_name = i
-        break
+
+def find_bot_file():
+    for file in os.listdir():
+        if file.endswith(".py") and file.startswith("bot_"):
+            return file
+    return None
+
+def stop_bot(file_name):
+    process_lines = os.popen("ps -ef").read().split("\n")
+    for line in process_lines:
+        if file_name in line:
+            columns = line.split()
+            prc_id_index = columns[1]
+            try:
+                prc_id = int(prc_id_index)
+                os.system(f"kill -9 {prc_id}")
+            except ValueError:
+                print(f"Error: Invalid process ID found in line: {line}")
+
+def delete_media_files():
+    media_extensions = {".mp3", ".mp4", ".mkv"}
+    for file in os.listdir():
+        if any(file.endswith(ext) for ext in media_extensions):
+            os.remove(file)
 
 
-for i in os.popen("ps -ef").read().split("\n"):
-    if file_name in i:
-            prc_id = i = i[9:17]
-            prc_id = int(prc_id)
-            r = os.popen("kill -9 "+str(prc_id))
+def main():
+    file_name = find_bot_file()
 
+    if file_name:
+        stop_bot(file_name)
+        delete_media_files()
+        print(f"{Style.BRIGHT}{GREEN}The bot has been stopped{PLAIN}")
 
-for i in os.listdir():
-    if i.endswith(".mp3") or i.endswith(".mp4") or i.endswith(".mkv") or i.endswith(".mp3"):
-        os.remove(i)
-
-print(f"{Style.BRIGHT}{GREEN}The bot has been Stopped{PLAIN}")
+if __name__ == "__main__":
+    main()
