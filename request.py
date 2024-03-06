3ab3bcdd5c98f3bc1735653d07e0badceb860037
Tele Dark <teledark0978@gmail.com>
2024-03-06 12:47:18 +0000

    improved organization and readability

--- request.py
+++ request.py
@@ -1,40 +1,41 @@
 import requests
-from time import time
-import time as tmdd
+from time import sleep
+
 class HTTPrequest:
-    def __init__(self,url):
-        self.con = self.create_connection(url)
-        self.response_code = self.con.status_code
-        self.headers = self.con.headers
-        lnd = self.headers.get("content-length")
-        self.buffer_size = (int(lnd) if lnd != None else None)
-    def create_connection(self,url,rcvd=None):
+    def __init__(self, url):
+        self.connection = self.create_connection(url)
+        self.response_code = self.connection.status_code
+        self.headers = self.connection.headers
+        content_length = self.headers.get("content-length")
+        self.buffer_size = int(content_length) if content_length else None
+
+    def create_connection(self, url, received=None):
         headers = {
-            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36"
         }
-        if rcvd:
-            headers["Range"] = "bytes="+str(rcvd)+"-"+str(self.buffer_size)
-        trd = 0
-        while True:
+        if received:
+            headers["Range"] = f"bytes={received}-{self.buffer_size}"
+
+        retries = 0
+        while retries < 5:
             try:
-                con = requests.get(url,stream=True,headers=headers,timeout=10,allow_redirects=True)
-                if con.status_code == 200:
-                    return con
+                connection = requests.get(url, stream=True, headers=headers, timeout=10, allow_redirects=True)
+                if connection.status_code == 200:
+                    return connection
                 else:
-                    trd += 1
-                    tmdd.sleep(2)
-            except:
-                trd += 1
-            if trd > 5:
-                raise Exception('cant connect !')
-    def download(self,file_name:str):
-        file_stream = open(file_name,'wb')
-        try:
-            for data in self.con.iter_content(chunk_size=500000):
-                file_stream.write(data)
-        except Exception as e:
-            self.con.close()
-            file_stream.close()
-            raise Exception("")
-        
-        file_stream.close()
+                    retries += 1
+                    sleep(2)
+                    
+            except requests.RequestException:
+                retries += 1
+
+        raise Exception('Unable to establish a connection!')
+
+    def download(self, file_name: str):
+        with open(file_name, 'wb') as file_stream:
+            try:
+                for data in self.connection.iter_content(chunk_size=500000):
+                    file_stream.write(data)
+            except Exception as e:
+                self.connection.close()
+                raise Exception("Download failed")
